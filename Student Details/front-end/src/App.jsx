e682902ce41b4a4a11ce3a8d6f51c3a20334015d
Hema-Nath-Reddy <164506740+Hema-Nath-Reddy@users.noreply.github.com>
2025-01-20 23:00:14 +0530

    Student Details Assignment

---
+++ Student Details/front-end/src/App.jsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import "./App.css";
+import StudentDetails from "./StudentDetails";
+
+function App() {
+  const [name, setName] = useState("");
+  const [number, setNumber] = useState("");
+  const [age, setAge] = useState("");
+  const [gender, setGender] = useState("");
+  const [data, setData] = useState([]);
+  
+  const handleSubmit = (e) => {
+    if (name === "" || number === "" || age === "" || gender === "") {
+      e.preventDefault();
+      return;
+    }
+    setName("");
+    setNumber("");
+    setAge("");
+    setGender("");
+
+    setData([...data, { name, number, age, gender }]);
+  };
+  const handleDelete = (index) => {
+    const newData = data.filter((item, i) => i !== index);
+    setData(newData);
+  };
+  return (
+    <>
+      <h1>Student details</h1>
+      <div className="form">
+        <input
+          type="text"
+          name="name"
+          id="name"
+          placeholder="Enter name"
+          autoComplete="name"
+          spellCheck="false"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input
+          type="tel"
+          name="number"
+          id="number"
+          placeholder="Enter number"
+          autoComplete="tel"
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+        <input
+          type="number"
+          name="age"
+          id="age"
+          min={16}
+          max={25}
+          placeholder="Enter age"
+          autoComplete="tel"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <select value={gender} onChange={(e) => setGender(e.target.value)}>
+          <option value="" disabled>
+            Select Gender
+          </option>
+          <option value="M">Male</option>
+          <option value="F">Female</option>
+        </select>
+
+        <button onClick={handleSubmit}>Add</button>
+      </div>
+
+      {data.map((item, index) => (
+        <StudentDetails
+          key={index}
+          {...item}
+          index={index + 1}
+          handleDelete={() => handleDelete(index)}
+        />
+      ))}
+    </>
+  );
+}
+
+export default App;
