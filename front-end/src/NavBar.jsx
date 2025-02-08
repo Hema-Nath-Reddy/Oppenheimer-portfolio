ccfd268fd578bf75f4a653d32f7f2aa1b5fbd24a
Hema-Nath-Reddy <164506740+Hema-Nath-Reddy@users.noreply.github.com>
2025-02-08 13:12:52 +0530

    full screen feature

--- front-end/src/NavBar.jsx
+++ front-end/src/NavBar.jsx
@@ -6,6 +6,16 @@ import audio from "./assets/song.mp3";
 const NavBar = () => {
   const [hoveredIndex, setHoveredIndex] = useState(null);
   const [clicked, setClicked] = useState(false);
+  const [isFullscreen, setIsFullscreen] = useState(false);
+  const toggleFullscreen = () => {
+    if (!document.fullscreenElement) {
+      document.documentElement.requestFullscreen();
+      setIsFullscreen(true);
+    } else {
+      document.exitFullscreen();
+      setIsFullscreen(false);
+    }
+  };
   const handleMouseEnter = (index) => {
     setHoveredIndex(index);
   };
@@ -134,6 +144,17 @@ const NavBar = () => {
           </a>
         ))}
       </div>
+      <button onClick={toggleFullscreen} className="screen">
+        {isFullscreen ? (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+            <path d="M240-120v-120H120v-80h200v200h-80Zm400 0v-200h200v80H720v120h-80ZM120-640v-80h120v-120h80v200H120Zm520 0v-200h80v120h120v80H640Z" />
+          </svg>
+        ) : (
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 -960 960 960">
+            <path d="M120-120v-200h80v120h120v80H120Zm520 0v-80h120v-120h80v200H640ZM120-640v-200h200v80H200v120h-80Zm640 0v-120H640v-80h200v200h-80Z" />
+          </svg>
+        )}
+      </button>
 
       <button
         className="sound"
